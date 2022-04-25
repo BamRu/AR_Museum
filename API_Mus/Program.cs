e1ea3c66125aa518e1bc6d7bb040ce817efb5298
CapSparkle <Feskovich.A@yandex.com>
2022-04-25 22:05:48 +0700

    Understanding complex code structures

--- API_Mus/Program.cs
+++ API_Mus/Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+//using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
 namespace API_Mus
 {
@@ -13,14 +13,23 @@ namespace API_Mus
     {
         public static void Main(string[] args)
         {
+            configureWebHostCallback += configureWebHost;
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+		{
+            IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args);
+
+            return hostBuilder.ConfigureWebHostDefaults(configureWebHostCallback);
+        }
+
+        public static Action<IWebHostBuilder> configureWebHostCallback;
+
+
+        public static void configureWebHost(IWebHostBuilder webHostBuilder)
+		{
+            webHostBuilder.UseStartup<Startup>();
+		}
     }
 }
